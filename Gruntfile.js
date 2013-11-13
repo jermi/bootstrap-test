6fb05650c786eca48572f10511fb9a8c2be0dd35
jermi <jermi@o2.pl>
2013-11-13 22:13:39 +0100

    spritesmith test

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,20 @@ module.exports = function (grunt) {
                 src : 'src/main/webapp/preprocessor_templates/index.html',
                 dest : 'src/main/webapp/index.html'
             }
-        }
+        },
+		sprite:{
+			all: {
+				src: 'src/main/webapp/**/*.png',
+				destImg: 'src/main/webapp/compressed/img.png',
+				destCSS: 'src/main/webapp/css/img.css',
+			    'algorithm': 'binary-tree',
+				'engine': 'phantomjs'
+			}
+		},
+		clean: [
+			'src/main/webapp/compressed/img.png', 
+			'src/main/webapp/css/img.css'
+		]
     });
 
     // loading tasks from plugins
@@ -62,10 +75,13 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint'); // js validation
     grunt.loadNpmTasks('grunt-env'); // provides ability to set enviromental variables
     grunt.loadNpmTasks('grunt-preprocess'); // processes page to use compressed or not compressed resources
+	grunt.loadNpmTasks('grunt-spritesmith');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
     // tasks
     grunt.registerTask('default', ['jshint']);
     grunt.registerTask('dev', ['jshint', 'env:dev', 'preprocess:dev']);
     grunt.registerTask('prod', ['jshint', 'env:prod', 'preprocess:prod', 'uglify', 'cssmin']);
+	grunt.registerTask('img', ['clean', 'sprite']);
 
 };
